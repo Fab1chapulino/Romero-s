c31a25ab8e76036c34f27460958a992b7c7bae07
Fabian Romero <chapulinozeta@gmail.com>
2023-07-03 13:30:26 -0400

    New formmating rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'airbnb-typescript/base',
   ],
   root: true,
   env: {
